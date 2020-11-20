6663e512aa2bf600d985a4a1551b2e91509d094d
Polyhistor <pouya.ataei.7@gmail.com>
2020-11-20 22:12:33 +1300

    added the express server, starting for clustering

---
+++ performance/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+function doWork(duration) {
+  const start = Date.now();
+  while (Date.now() - start < duration) {}
+}
+
+app.get('/', (req, res) => {
+  res.send('Hi there');
+});
+
+app.listen(3000);
