0f3c5fc97ed68de3efa3ac075df0c58dc6433ccd
Polyhistor <pouya.ataei.7@gmail.com>
2020-11-21 12:53:38 +1300

    added the basic cluster master

--- performance/index.js
+++ performance/index.js
@@ -1,13 +1,23 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-function doWork(duration) {
-  const start = Date.now();
-  while (Date.now() - start < duration) {}
+// is the file being executed in master mode?
+if (cluster.isMaster) {
+  // this cause Index.js to be executed again, but in a slave/child mode
+  cluster.fork();
+} else {
+  // I'm a child, I'm going to act like a server and do nothing else
+  const express = require('express');
+  const app = express();
+
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+  }
+
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('Hi there');
+  });
+
+  app.listen(3000);
 }
-
-app.get('/', (req, res) => {
-  res.send('Hi there');
-});
-
-app.listen(3000);
